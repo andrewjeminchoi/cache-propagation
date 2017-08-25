36b26c409875e1aef4f1f6cbb251cedaf6dea977
Andrew Choi <andrewchoi96@hotmail.com>
2017-08-25 15:19:48 -0700

    tested for simple add circuit

--- ac_to_ac.py
+++ ac_to_ac.py
@@ -12,15 +12,18 @@ import math
 class node:
     # Node type can be 'n' or 'v' for leaf nodes
     # Node type can be '+' or '*' for non-leaf
-    def __init__(self, ntype, index, value, derivative):
+    def __init__(self, ntype, index, var, value, derivative):
         self.ntype = ntype
-        self.index = index # denotes the n'th variable
+        self.nIndex = index
+        self.var = var # denotes the n'th variable
         self.vr = value
         self.dr = derivative
         self.numChild = 0 # number of child nodes of non-leaf
         self.childList = [] # indices of children
         self.prL = [] # product cache register
         self.prR = []
+        self.childValues = [] # values of children
+        self.line = 0 # Line in output AC
 
     # Helper functions
     def update_vr(self, newVal):
@@ -28,6 +31,9 @@ class node:
 
     def update_dr(self, newDr):
         self.dr = newDr
+
+    def update_childList(self, child):
+        self.childList = child
 
     def add_child(self, child):
         self.childList.append(child)
@@ -41,19 +47,33 @@ class node:
     def update_prR(self, cache):
         self.prR = cache
 
+    def update_childValues(self, cVal):
+        self.childValues = cVal
+
+# Stores constant pairs to be inserted into AC
+class ACNode:
+     def __init__(self, n, i):
+         self.n = n # node
+         self.index = i # line number in AC
+
+# Pairs the index of node with a constant 
+class Container:
+    def __init__(self, ind, obj):
+        self.index = ind
+        self.constant = obj
 
 # Check if file was passed by command line
 FILE_NAME = ''
 FILE_SIZE = 0
-if (len(sys.argv) < 3):
+if (len(sys.argv) < 2):
     FILE_NAME = input("Please input an AC file: ")
-    FILE_SIZE = input("Please input the AC size: ")
+    # FILE_SIZE = input("Please input the AC size: ")
 else:    
     FILE_NAME = sys.argv[1]
-    FILE_SIZE = sys.argv[2]
+    #FILE_SIZE = sys.argv[2]
 
 print("File name:", FILE_NAME)
-print("size:", FILE_SIZE)
+#print("size:", FILE_SIZE)
 
 # List to add nodes
 circuit = []
@@ -68,25 +88,26 @@ for line in AC_FILE:
     elif line[0] == 'E':
         print("\t ... done reading file ...")
         index = index - 1
-        circuit[index].dr = 1
+        #circuit[index].update_dr(1)
         break
     else:
         parsed = line.split()
+        
         if parsed[0] == 'n':
             # index and dr equals 0
             val = float(parsed[1])
-            circuit.append(node(parsed[0], 0, val, 0))
+            circuit.append(node(parsed[0], index, 0, val, 0))
             index = index + 1
             
         elif parsed[0] == 'v':
-            ind = int(parsed[1])
+            var = int(parsed[1])
             val = float(parsed[2])
-            circuit.append(node(parsed[0], ind, val, 0))
+            circuit.append(node(parsed[0], index, var, val, 0))
             index = index + 1
 
         elif parsed[0] == '+':
             value = 0
-            addNode = node(parsed[0], 0, 0, 0)
+            addNode = node(parsed[0], index, 0, 0, 0)
             for child in parsed:
                 if child == '+':
                     #nothing
@@ -102,8 +123,8 @@ for line in AC_FILE:
         elif parsed[0] == '*':
             value = 1.0
             numChild = 0
-            multNode = node(parsed[0], 0, 0, 0)
-            childValues = [1.0]
+            multNode = node(parsed[0], index, 0, 0, 0)
+            childValues = [1.0] # first value is a placeholder
             prL = []
             prR = []
             for child in parsed:
@@ -127,27 +148,120 @@ for line in AC_FILE:
                 productR = childValues[k] * prR[j-1]
                 prR.append(productR)
                 k = k - 1
-            print("L", prL)
-            print("R", prR)
+
             value = prL[numChild]
             
             multNode.update_prL(prL)
             multNode.update_prR(prR)
             multNode.update_vr(value)
+            multNode.update_childValues((childValues.pop(0)))
             circuit.append(multNode)
             index = index + 1
 
     #print(index)
-for i in circuit:
-    #print(i.vr)
-    break
+
+# Close AC file
+AC_FILE.close()
 
 outputVal = circuit[index].vr
+circuit[index].update_dr(1)
 
 print("output:", outputVal)
 print("output log", math.log10(outputVal))
 
-# Write to a test file
+outputAC = ''
 
-# Close AC file
-AC_FILE.close()
+# Start cache-backpropagation
+
+''' 
+The leaf index is the index of the node in the circuit
+We will find the marginal for this index
+'''
+leafIndex = int(input("Find marginal of leaf node index: "))
+
+counter = 0 # counts the line/node number in the ac
+isDone = False 
+
+# dict storing the nodes for the backpropagation circuit
+backCircuit = dict() # Pair: (index in original circuit, ACNode)
+
+# Reverse the original circuit for "backpropagation"
+reversedCircuit = list(circuit)
+reversedCircuit.reverse()
+
+for parent in reversedCircuit:
+    # Output all dr values, pr values as constants
+
+    # parent dr
+    #parent.line = counter
+    
+    #ACParent = ACNode((parent), counter)
+    #parentDr = (ACParent.n).dr
+    #outputAC += 'n ' + str(parent.dr) + '\n'
+    #counter += 1
+    #backCircuit[index] = ACParent
+    
+
+    
+    if parent.ntype == '+':
+        parent.line = counter
+        outputAC += 'n ' + str(parent.dr) + '\n'
+        counter += 1
+        for childIndex in parent.childList:
+            childNode = circuit[childIndex]
+                        
+            if leafIndex in parent.childList:
+                # Create a node for child dr
+                if childNode.line == 0: # child was not assigned a line
+                    childNode.line = counter
+                    outputAC += 'n ' + str(childNode.dr) + '\n'
+                    counter += 1
+                childNode.dr += parent.dr
+                parentLine = str(parent.line)
+                childLine = str(childNode.line)
+                outputAC += '+ ' + parentLine + ' ' + childLine + '\n'
+                counter += 1
+                isDone = True
+                break
+
+            else: 
+                childNode.dr += parent.dr
+                # Check if the child node is in output AC
+                if childNode.line == 0:
+                    childNode.line = parent.line
+                else:
+                    # Insert the add node in output AC
+                    parentLine = str(parent.line)
+                    childLine = str(childNode.line)
+                    outputAC += '+ ' + parentLine + ' ' + childLine + '\n'
+                    counter += 1
+
+    elif parent.ntype == '*':
+        parent.line = counter
+        outputAC += 'n ' + str(parent.dr) + '\n'
+        counter += 1
+
+    if isDone:
+        break
+'''
+    # parent product cache
+    for i in parent.prL:
+        cacheVal = Constant(i, counter)
+        outputAC += 'n ' + str(i) + '\n'
+        backCircuit.append(cacheVal)
+        counter += 1
+
+    for i in parent.prR:
+        cacheVal = Constant(i, counter)
+        outputAC += 'n ' + str(i) + '\n'
+        backCircuit.append(cacheVal)
+        counter += 1 
+    '''
+    #index -= 1 # decrement the index (node position in circuit)
+    
+outputAC += 'EOF'
+
+
+print(outputAC)
+
+# Write to a test file
