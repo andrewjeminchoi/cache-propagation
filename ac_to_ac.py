2102ca6f3d2edd3def5e2acfb21d8cc54c173f6c
Andrew Choi <andrewchoi96@hotmail.com>
2017-08-25 21:22:30 -0700

    Working prototype
    AC output finds the marginal of rightmost node in highest depth branch.

--- ac_to_ac.py
+++ ac_to_ac.py
@@ -8,6 +8,11 @@
 import sys
 import math
 
+# Global Variables
+# List to add nodes
+circuit = []
+index = 0
+
 # Node Class
 class node:
     # Node type can be 'n' or 'v' for leaf nodes
@@ -56,12 +61,9 @@ class ACNode:
          self.n = n # node
          self.index = i # line number in AC
 
-# Pairs the index of node with a constant 
-class Container:
-    def __init__(self, ind, obj):
-        self.index = ind
-        self.constant = obj
+# Functions
 
+        
 # Check if file was passed by command line
 FILE_NAME = ''
 FILE_SIZE = 0
@@ -75,9 +77,7 @@ else:
 print("File name:", FILE_NAME)
 #print("size:", FILE_SIZE)
 
-# List to add nodes
-circuit = []
-index = 0
+
 
 # Read the AC file
 AC_FILE = open(FILE_NAME, 'r')
@@ -98,7 +98,7 @@ for line in AC_FILE:
             val = float(parsed[1])
             circuit.append(node(parsed[0], index, 0, val, 0))
             index = index + 1
-            
+
         elif parsed[0] == 'v':
             var = int(parsed[1])
             val = float(parsed[2])
@@ -169,47 +169,31 @@ circuit[index].update_dr(1)
 print("output:", outputVal)
 print("output log", math.log10(outputVal))
 
-outputAC = ''
-
-# Start cache-backpropagation
-
 ''' 
+Start cache-backpropagation
 The leaf index is the index of the node in the circuit
 We will find the marginal for this index
 '''
 leafIndex = int(input("Find marginal of leaf node index: "))
-
 counter = 0 # counts the line/node number in the ac
-isDone = False 
-
-# dict storing the nodes for the backpropagation circuit
-backCircuit = dict() # Pair: (index in original circuit, ACNode)
+isDone = False # breaks out of loop if goal node is reached
+outputAC = '' # Output string
 
 # Reverse the original circuit for "backpropagation"
 reversedCircuit = list(circuit)
 reversedCircuit.reverse()
 
+outputAC += "(TBA)\n" # Placeholder
+
 for parent in reversedCircuit:
-    # Output all dr values, pr values as constants
-
-    # parent dr
-    #parent.line = counter
-    
-    #ACParent = ACNode((parent), counter)
-    #parentDr = (ACParent.n).dr
-    #outputAC += 'n ' + str(parent.dr) + '\n'
-    #counter += 1
-    #backCircuit[index] = ACParent
-    
-
-    
     if parent.ntype == '+':
         parent.line = counter
         outputAC += 'n ' + str(parent.dr) + '\n'
         counter += 1
         for childIndex in parent.childList:
             childNode = circuit[childIndex]
-                        
+
+            # End program if we reached goal node
             if leafIndex in parent.childList:
                 # Create a node for child dr
                 if childNode.line == 0: # child was not assigned a line
@@ -224,44 +208,65 @@ for parent in reversedCircuit:
                 isDone = True
                 break
 
+            # Continue - we haven't reached goal node
             else: 
                 childNode.dr += parent.dr
+                
                 # Check if the child node is in output AC
                 if childNode.line == 0:
+                    # Saves one edge by redirecting the child dr to parent
                     childNode.line = parent.line
                 else:
+                    # The child node is already in the AC
                     # Insert the add node in output AC
                     parentLine = str(parent.line)
                     childLine = str(childNode.line)
                     outputAC += '+ ' + parentLine + ' ' + childLine + '\n'
+                    childNode.line = counter # Change the line to the add node
                     counter += 1
 
     elif parent.ntype == '*':
+        pos = 1 # Keep track of node position in cache
+        
         parent.line = counter
         outputAC += 'n ' + str(parent.dr) + '\n'
         counter += 1
+        
 
+        for childIndex in parent.childList:
+            childNode = circuit[childIndex]
+            w = parent.numChild
+
+            # Calculate dr
+            rightVal = parent.prR[(w-pos)]
+            leftVal = parent.prL[(pos-1)]
+            childNode.dr += parent.dr * rightVal * leftVal
+            pos += 1
+
+            # Insert constant nodes for cache value
+            prLLine = str(counter)
+            counter += 1
+            outputAC += 'n ' + str(rightVal) + '\n'
+            prRLine = str(counter)
+            counter += 1
+            outputAC += 'n ' + str(leftVal) + '\n'
+
+            parentLine = str(parent.line)
+
+            # Insert multiplication node
+            outputAC += '* ' + prLLine + ' ' + prRLine + ' ' + parentLine + '\n'
+            counter += 1
+        
     if isDone:
         break
-'''
-    # parent product cache
-    for i in parent.prL:
-        cacheVal = Constant(i, counter)
-        outputAC += 'n ' + str(i) + '\n'
-        backCircuit.append(cacheVal)
-        counter += 1
 
-    for i in parent.prR:
-        cacheVal = Constant(i, counter)
-        outputAC += 'n ' + str(i) + '\n'
-        backCircuit.append(cacheVal)
-        counter += 1 
-    '''
     #index -= 1 # decrement the index (node position in circuit)
     
 outputAC += 'EOF'
 
-
-print(outputAC)
+#print(outputAC)
 
 # Write to a test file
+f = open('out.ac', 'w')
+f.write(outputAC)
+f.close()
